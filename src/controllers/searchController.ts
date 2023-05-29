d2a6fd55b8389c758cd61f1a86473c9e482179dc
Yuri Silveira <yuri.pires.silveira@gmail.com>
2023-05-29 13:05:55 -0300

    ativando o menu e listando os pets no controller

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
+import { Pet } from "../models/pets";
 
 export const search = (req: Request, res: Response) => {
-
+    let list = Pet.getFromName(req.params.name)
+    res.render('pages/page',{list})
 }
