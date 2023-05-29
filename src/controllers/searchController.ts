022f1828d8d51559aad8cec4d72e4e5db4ded445
Yuri Silveira <yuri.pires.silveira@gmail.com>
2023-05-29 13:27:24 -0300

    final project 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -2,6 +2,11 @@ import { Request, Response } from "express";
 import { Pet } from "../models/pets";
 
 export const search = (req: Request, res: Response) => {
-    let list = Pet.getFromName(req.params.name)
-    res.render('pages/page',{list})
+    let name: string = req.query.q as string
+    let list = Pet.getFromName(name)
+
+    res.render('pages/page',{
+        list,
+        name
+    })
 }
